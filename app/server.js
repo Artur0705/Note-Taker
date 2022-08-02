2bc173056e173a72401696ea73e23d1da5e8e4e6
Artur0705 <artlil420@gmail.com>
2022-08-02 15:16:51 +1000

    Requirin modules, setting up a port, creating reusable functions, adding gitignore file

---
+++ app/server.js
@@ -0,0 +1,27 @@
+// Requiring modules
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const uuid = require("uuid");
+
+//Declaring port to any or 3001
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+
+// Using a middleware for public files
+app.use(express.static("public"));
+
+const getFile = (file) => {
+  return path.join(__dirname, file);
+};
+
+const getHtml = (name) => {
+  return getFile(`../public/${name}.html`);
+};
+
+const getDb = () => {
+  return getFile(`../db/db.json`);
+};
